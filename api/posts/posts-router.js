f6e124921ddf85e56ea56ceb783bb65f15f6aabd
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-06 12:25:33 -0400

    working on posts-router.js endpoints

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router();
+
+// Posts Endpoints
+
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ 
+        message: 'The posts information could not be retrieved',
+        error: err
+       });
+    });
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+});
+
+router.delete('/:id', (req, res) => {
+});
+
+router.get('/:id/comments', (req, res) => {
+});
--- api/server.js
+++ api/server.js
@@ -1,2 +1,10 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
 // require your posts router and connect it here
+const postsRouter = require('./api/posts/posts-router.js');
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+module.exports = server;
