c9156b8a0139d06ac33527ea9f834a39461c7b8a
Felipe Slaughter-Quintero <fslauq@gmail.com>
2021-08-07 13:28:35 -0400

    .

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 // implement your server here
 const express = require('express');
+const cors = require('cors');
 const postsRouter = require('./posts/posts-router.js');
 const server = express();
 
 // require your posts router and connect it here
 server.use(express.json());
+server.use(cors());
 server.use("/api/posts", postsRouter);
 
 module.exports = server;
